36586c5b2cb2223a6b5ed75cfe5080d75f50ca64
Aarim02 <akim2002@utexas.com>
2023-08-08 18:10:53 -0500

    Changed input variables
    Increased max input and increased input smoothing

--- script/lpf.py
+++ script/lpf.py
@@ -14,13 +14,13 @@ class vel_smoother:
         input_linear,input_angular = vector3_convertor(v3_linear),vector3_convertor(v3_angular)
         alpha = [0,0]
         if (input_linear==0).all() == True:
-            alpha[0] = 0.05
+            alpha[0] = 0.009
         else:
-            alpha[0] = 0.005
+            alpha[0] = 0.002
         if (input_angular==0).all() == True:
-            alpha[1] = 0.05
+            alpha[1] = 0.009
         else:
-            alpha[1] = 0.01       
+            alpha[1] = 0.004       
         new_linear = alpha[0]*input_linear + (1-alpha[0])*self.linear
         new_angular = alpha[1]*input_angular + (1-alpha[1])*self.angular
         out_linear, out_angular = Vector3(float(new_linear[0]),float(new_linear[1]),float(new_linear[2])), Vector3(float(new_angular[0]),float(new_angular[1]),float(new_angular[2]))
